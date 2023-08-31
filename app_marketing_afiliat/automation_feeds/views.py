d46ebc462a8d7297cf915b6baafefec62e789303
andrei.reporter13@gmail.com <andrei.reporter13@gmail.com>
2023-09-01 00:47:50 +0300

    add before load_more_js

--- app_marketing_afiliat/automation_feeds/views.py
+++ app_marketing_afiliat/automation_feeds/views.py
@@ -2,10 +2,28 @@
 #
 #
 from django.views.generic import TemplateView
+from django.http import JsonResponse
+from .models import Feeds
+
+
+# process feeds here
+def process_feeds(feeds):
+    for product in feeds:
+        if '-' in product.title:
+            product.split_title = product.title.split('-')[0]
+        else:
+            product.split_title = product.title
 
 
 class HomePageView(TemplateView):
     template_name = 'automation_feeds/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        feeds = Feeds.objects.all()[:8]
+        process_feeds(feeds)
+        context['feeds'] = feeds
+        return context
 
 
 class PromotionsPageView(TemplateView):
@@ -14,6 +32,13 @@ class PromotionsPageView(TemplateView):
 
 class FeedsPageView(TemplateView):
     template_name = 'automation_feeds/feeds.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        feeds = Feeds.objects.all()[:8]
+        process_feeds(feeds)
+        context['feeds'] = feeds
+        return context
 
 
 class ShopsPageView(TemplateView):
