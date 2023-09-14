5bf47edda5a926419e4f06e56d4e8e5804de2bf0
andreireporter13 <andrei.reporter13@gmail.com>
2023-09-15 00:19:12 +0300

    added functional blog app

--- app_marketing_afiliat/app_marketing_afiliat/urls.py
+++ app_marketing_afiliat/app_marketing_afiliat/urls.py
@@ -14,3 +14,6 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.)$', serve, {'document_root': settings.STATIC_ROOT}, name='static'),
     re_path(r'^media/(?P<path>.)$', serve, {'document_root': settings.MEDIA_ROOT}, name='media'),
     ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app_marketing_afiliat/blog_affiliate/urls.py
+++ app_marketing_afiliat/blog_affiliate/urls.py
@@ -3,16 +3,9 @@
 #
 from django.urls import path
 from . import views
-#
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.BlogPageView.as_view(), name="blog"),
     path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
-
-# for media
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
