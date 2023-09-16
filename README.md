e137ec5d31ba65ba7769d9f7c89c09e11c4110fe
andreireporter13 <andrei.reporter13@gmail.com>
2023-09-16 09:45:39 +0300

    added test for blog

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-### Ongoing description...
+# Proiectul de Marketing Afiliat Automatizat
+
+Acest proiect este o aplicație web dezvoltată cu Django care automatizează marketingul afiliat și actualizarea produselor afiliate.
+
+1. [Introducere](#introducere)
+2. [Configurarea Mediului de Dezvoltare](#configurarea-mediului-de-dezvoltare)
+3. [Structura Proiectului](#structura-proiectului)
+4. [Modele de Date](#modele-de-date)
+5. [Administrație](#administrație)
+6. [Views](#views)
+7. [URL-uri](#url-uri)
+8. [Template-uri](#template-uri)
+10. [Procesarea Produselor Afiliate](#procesarea-produselor-afiliate)
+11. [Sincronizarea automată a Produselor](#sincronizarea-automata-a-produselor)
+12. [Încheiere](#incheiere)
--- app_marketing_afiliat/blog_affiliate/tests.py
+++ app_marketing_afiliat/blog_affiliate/tests.py
@@ -1,3 +1,37 @@
+#
+#
+#
+#
+#
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
 
-# Create your tests here.
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        Post.objects.create(
+            title='Affiliate Marketing',
+            content='Affiliate Marketing test content. Awesome.',
+            author=user,
+            slug='affiliate-marketing',
+            published_date=None
+        )
+
+    def test_title_max_length(self):
+        post = Post.objects.get(id=1)
+        max_length = post._meta.get_field('title').max_length
+        self.assertEqual(max_length, 200)
+
+    def test_slug(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.slug, 'affiliate-marketing')
+
+    def test_get_absolute_url(self):
+        post = Post.objects.get(id=1)
+        self.assertEqual(post.get_absolute_url(), '/blog/post/affiliate-marketing/')
